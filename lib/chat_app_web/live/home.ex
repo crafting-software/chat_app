4c999b9d24f12488c59caee24a8b65df5c7e2a2d
Tania Sasaran <taniasasaran@gmail.com>
2023-04-13 15:43:52 +0300

    feat(3): added join links

--- lib/chat_app_web/live/home.ex
+++ lib/chat_app_web/live/home.ex
@@ -2,7 +2,7 @@ defmodule ChatAppWeb.Home do
   use ChatAppWeb, :live_view
 
   def mount(_params, _session, socket) do
-    rooms = [%{id: 1, name: "room1"}, %{id: 2, name: "room2"}, %{id: 3, name: "room3"}]
+    rooms = [%{id: 1, name: "room1", description: "capybara numero uno"}, %{id: 2, name: "room2", description: "capybara numero dos"}, %{id: 3, name: "room3", description: "capybara numero tres"}]
     socket = assign(socket, :rooms, rooms)
     {:ok, socket}
   end
@@ -15,6 +15,12 @@ defmodule ChatAppWeb.Home do
     <.table id="rooms" rows={@rooms}>
       <:col :let={room}><%= room.id %></:col>
       <:col :let={room}><%= room.name %></:col>
+      <:col :let={room}><%= room.description %></:col>
+      <:action>
+        <.link method="join">
+          Join
+        </.link>
+      </:action>
     </.table>
 
     <form class="jn">
@@ -23,8 +29,8 @@ defmodule ChatAppWeb.Home do
     </form>
 
     <div class="drawing">
-      <div class="drawn"></div>
-
+      <div class="draw" id="jos"></div>
+      <div class="draw" id="sus"></div>
     </div>
     """
   end
