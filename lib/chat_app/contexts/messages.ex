ac22dbb81ed5d39533b1570da8d24a2d96ad22ef
EndlessRecurrence <georgesut@yahoo.com>
2023-05-18 09:49:56 +0300

    feat(16): added reactions pop-up/Etso insertion
    Emojis are inserted into the :reactions association of a message. They're not displayed yet.

--- lib/chat_app/contexts/messages.ex
+++ lib/chat_app/contexts/messages.ex
@@ -16,6 +16,14 @@ defmodule ChatApp.Contexts.Messages do
     |> Repo.insert()
   end
 
+  def insert_message_reaction(%Message{} = message, reaction) do
+    IO.inspect reaction, label: "Reaction from the context"
+    message
+    |> Ecto.Changeset.change()
+    |> Ecto.Changeset.put_assoc(:reactions, [reaction | message.reactions])
+    |> Repo.update()
+  end
+
   def delete_message(%Message{} = message), do: Repo.delete(message)
 
   def create_message_as_map(content, sender, room_id) do
